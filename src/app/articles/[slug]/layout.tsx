e5fc4f8d1bff807e316e93193e9c11d418b92d57
M. Scott Ford <scott@mscottford.com>
2026-01-05 08:37:01 -0500

    Integrates `content-collections` using dynamic imports
    Based on instructions found at https://www.content-collections.dev/samples/next-mdx-dynamic-import

---
+++ src/app/articles/[slug]/layout.tsx
@@ -0,0 +1,48 @@
+import { AppContext } from '@/app/providers'
+import { Container } from '@/components/Container'
+import { Prose } from '@/components/Prose'
+import { type ArticleWithSlug } from '@/lib/articles'
+import { formatDate } from '@/lib/formatDate'
+import { Post } from 'content-collections'
+
+function ArrowLeftIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+      <path
+        d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default function ArticleLayout({
+  article,
+  children,
+}: {
+  article: Post
+  children: React.ReactNode
+}) {
+  const previousPathname = false;
+  return (
+    <Container className="mt-16 lg:mt-32">
+      <div className="xl:relative">
+        <div className="mx-auto max-w-2xl">
+          {previousPathname && (
+            <button
+              type="button"
+              /*onClick={() => router.back()}*/
+              aria-label="Go back to articles"
+              className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+            >
+              <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+            </button>
+          )}
+          {children}
+        </div>
+      </div>
+    </Container>
+  )
+}
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,10 +2,11 @@ import { type Metadata } from 'next'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
 
-function Article({ article }: { article: ArticleWithSlug }) {
+import { allPosts, Post } from "content-collections";
+
+function Article({ article }: { article: Post }) {
   return (
     <article className="md:grid md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
@@ -14,21 +15,21 @@ function Article({ article }: { article: ArticleWithSlug }) {
         </Card.Title>
         <Card.Eyebrow
           as="time"
-          dateTime={article.date}
+          dateTime={article.createdAt}
           className="md:hidden"
           decorate
         >
-          {formatDate(article.date)}
+          {formatDate(article.createdAt)}
         </Card.Eyebrow>
         <Card.Description>{article.description}</Card.Description>
         <Card.Cta>Read article</Card.Cta>
       </Card>
       <Card.Eyebrow
         as="time"
-        dateTime={article.date}
+        dateTime={article.createdAt}
         className="mt-1 max-md:hidden"
       >
-        {formatDate(article.date)}
+        {formatDate(article.createdAt)}
       </Card.Eyebrow>
     </article>
   )
@@ -41,8 +42,6 @@ export const metadata: Metadata = {
 }
 
 export default async function ArticlesIndex() {
-  let articles = await getAllArticles()
-
   return (
     <SimpleLayout
       title="Writing on software design, company building, and the aerospace industry."
@@ -50,8 +49,8 @@ export default async function ArticlesIndex() {
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
-          {articles.map((article) => (
-            <Article key={article.slug} article={article} />
+          {allPosts.map((article) => (
+            <Article key={article._meta.path} article={article} />
           ))}
         </div>
       </div>
