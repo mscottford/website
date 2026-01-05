ef161969d6bc3446868501b29ff102cbef225f7d
M. Scott Ford <scott@mscottford.com>
2026-01-05 08:37:01 -0500

    Conditionally configure for static exports

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,17 @@ import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
+const staticExportOptions = 
+  process.env.STATIC_EXPORT === 'true' ? {
+    output: 'export',
+    trailingSlash: true,
+    basePath: process.env.PAGES_BASE_PATH,
+  } : {}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...staticExportOptions,
+
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   outputFileTracingIncludes: {
     '/articles/*': ['./src/app/articles/**/*.mdx'],
