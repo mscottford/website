0aa941be1619dbfb83df939112040bea35cbe450
M. Scott Ford <scott@mscottford.com>
2026-01-10 23:54:15 -0500

    Uses a snippet for the tagline on the articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -5,6 +5,7 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 import { formatDate } from '@/lib/formatDate'
 
 import { allPosts, Post } from "content-collections";
+import { getSnippetPlain } from '@/lib/snippets'
 
 function Article({ article }: { article: Post }) {
   return (
@@ -37,8 +38,7 @@ function Article({ article }: { article: Post }) {
 
 export const metadata: Metadata = {
   title: 'Articles',
-  description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+  description: getSnippetPlain('articles-tagline'),
 }
 
 export default async function ArticlesIndex() {
@@ -48,8 +48,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Writing on software mending, programming, and some random bits just for fun."
+      intro={getSnippetPlain('articles-tagline')}
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
